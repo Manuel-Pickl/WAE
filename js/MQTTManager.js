5beb6ce7c4202ee281a2f6fcf8d3a04d817531a2
Manuel Pickl <picklmanuel98@gmail.com>
2023-04-18 11:35:52 +0200

    initial commit

---
+++ js/MQTTManager.js
@@ -0,0 +1,83 @@
+/**
+ * Handles the MQTT logic like connection and subscription
+ */
+ class MQTTManager {
+    /*
+        source: https://www.eclipse.org/paho/index.php?page=clients/js/index.php
+        test broker: http://www.hivemq.com/demos/websocket-client/;
+    */
+    host;
+    port;
+    mqtt;
+    subTopic;
+    pubTopic;
+
+    /**
+     * Creates and initializes an MQTTManager with host and port.
+     */
+    constructor(host, port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    /**
+     * Connects to the broker at the given ip address.
+     */
+    connect() {
+        // initialize broker connection
+        this.mqtt = new Paho.MQTT.Client(this.host, this.port, "clientId-5D50T7XTPW");
+        this.mqtt.onMessageArrived = this.onMessageArrived.bind(this);
+
+        var options = {
+            timeout: 3,
+            onSuccess: this.onConnect.bind(this),
+            onFailure: this.onFailure.bind(this),
+            useSSL: false
+        };
+        this.mqtt.connect(options);
+    }
+    
+    /**
+     * Triggers a console log on connection success to the broker & subscribes to the broker at the given topic
+     */
+    onConnect() {
+        console.log(`Connected to broker at: ${this.host}:${this.port}`);
+        this.subscribe();
+    }
+    
+    /**
+     * Triggers a console log on connection failure to the broker
+     */
+    onFailure() {
+        console.log(`No broker at: ${this.host}:${this.port}`);
+    }
+    
+    /**
+     * Subscribe to the broker
+     */
+    subscribe() {
+        this.mqtt.subscribe(this.subTopic);
+        console.log(`subscribed to topic ${this.subTopic}`);
+    }
+
+    /**
+     * Triggers when the broker publishes a message and saves it
+     * @param {*} message The published message from the broker
+     */
+    onMessageArrived(message) {
+        console.log(`recieved on topic "${message.destinationName}": "${message.payloadString}"`);
+        onEnemyMove(message.payloadString);
+    }
+
+    /**
+     * Publish a message to the broker.
+     * @param {string} message The message that gets published to the broker
+     */
+    publishMessage(message) {
+        let mqttMessage = new Paho.MQTT.Message(message);
+        mqttMessage.destinationName = this.pubTopic;
+        this.mqtt.send(mqttMessage);
+        
+        console.log(`published on topic "${this.pubTopic}": "${message}"`);
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,70 @@
+var playerNumber, enemyNumber;
+var playerColor, enemyColor;
+
+var mqttManager;
+const host = "broker.mqttdashboard.com";
+const port = 8000;
+
+
+// bind events
+document.querySelectorAll("button").forEach(button => {
+    button.onclick = () => {
+        // select player
+        playerNumber = button.id;
+        console.log("selected player", playerNumber);
+        
+        startGame();
+    }
+});
+
+document.querySelectorAll("td").forEach(td => {
+    td.onclick = () => onPlayerMove(td.id);
+});
+
+
+
+function startGame() {
+    changeToGameBoard();
+    initializeVariables();
+    initializeBroker();
+    console.log("game initialized");
+}
+
+function changeToGameBoard() {
+    // hide buttons & make board visible
+    document.querySelectorAll("button").forEach(button => button.style.display = "none");
+    document.querySelector("table").style.visibility = "visible";
+}
+
+function initializeVariables() {
+    // determine player/enemy number & color
+    enemyNumber = 3 - playerNumber;
+    playerColor = playerNumber == 1 ? "blue" : "red";
+    enemyColor = playerNumber == 1 ? "red" : "blue";
+}
+
+function initializeBroker() {
+    // initialize broker
+    mqttManager = new MQTTManager(host, port);
+    let topic = "uni/software-project";
+    mqttManager.pubTopic = `${topic}/${playerNumber}`;
+    mqttManager.subTopic = `${topic}/${enemyNumber}`;
+
+    // connect to broker
+    mqttManager.connect();
+}
+
+
+
+function onPlayerMove(tileNumber) {
+    // publish move
+    mqttManager.publishMessage(tileNumber);
+
+    // color tile
+    document.getElementById(tileNumber).style.background = playerColor;
+}
+
+function onEnemyMove(tileNumber) {
+    // color tile
+    document.getElementById(tileNumber).style.background = enemyColor;
+}
